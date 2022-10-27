ff78b4aaae54fbeef5f33d62b097ac0f133fbec7
Stas-0k <brickbybrick15@gmail.com>
2022-10-27 22:33:38 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-const galleryContainer = document.querySelector('.gallery')
+const galleryContainer = document.querySelector(".gallery");
 
-
-galleryContainer.innerHTML = galleryItems.map(({preview,original,description}) =>
-{
+galleryContainer.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
     return `<div class="gallery__item">
     <a class = "gallery__link" href="${original}">
     <img class="gallery__image"
     src=${preview} data-source=${original}
-    alt=${description}/></a></div>`
-}).join('')
+    alt=${description}/></a></div>`;
+  })
+  .join("");
 
+galleryContainer.addEventListener("click", onClick);
 
-galleryContainer.addEventListener("click", onClick)
-
-
-
-
-function onClick(event) { 
-    event.preventDefault()
-    const urlOriginal = event.target.dataset.source;
-    //console.log(urlOriginal)
-    const instance = basicLightbox.create(`
-    <img src="${urlOriginal}" width="800" height="600">
-`)
-
-    instance.show()
-    console.log(imgSrc.dataset.source)
+function onClick(event) {
+  event.preventDefault();
+  const urlOriginal = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${urlOriginal}" width=100% height=100%>`
+  );
+  showFunc(instance);
 }
 
+function showFunc(instance) {
+  instance.show();
+  if (instance.show()) {
+    addKeydown(instance)
+  }
+}
+
+function addKeydown(instance) { 
+    galleryContainer.addEventListener("keydown", onEsc);
+    function onEsc(event) {
+      if (event.code === "Escape") {
+        instance.close();
+        galleryContainer.removeEventListener("keydown", onEsc);
+      }
+    }
+}
