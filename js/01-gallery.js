2de8688172524244858b95d0184395012c935487
Stas-0k <brickbybrick15@gmail.com>
2022-10-27 16:36:50 +0300

    commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+
+
+galleryContainer.innerHTML = galleryItems.map(({preview,original,description}) =>
+{
+    return `<div class="gallery__item">
+    <a class = "gallery__link" href="${original}">
+    <img class="gallery__image"
+    src=${preview} data-source=${original}
+    alt=${description}/></a></div>`
+}).join('')
+
+
+galleryContainer.addEventListener("click", onClick)
+
+
+
+
+function onClick(event) { 
+    event.preventDefault()
+    const urlOriginal = event.target.dataset.source;
+    //console.log(urlOriginal)
+    const instance = basicLightbox.create(`
+    <img src="${urlOriginal}" width="800" height="600">
+`)
+
+    instance.show()
+    console.log(imgSrc.dataset.source)
+}
+
