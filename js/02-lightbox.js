1fd0a297a7e36d2e62156309fbfe0c14bf5240fa
Stas-0k <brickbybrick15@gmail.com>
2022-10-27 23:47:50 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,12 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+galleryContainer.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class = "gallery__item" href="${original}">
+    <img class="gallery__image"
+    src=${preview}
+    alt=${description}/></a>`;
+  })
+  .join("");
