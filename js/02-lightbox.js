bc8bc7c26eb59c0d12d2805b6d43f46ab3833571
Stas-0k <brickbybrick15@gmail.com>
2022-10-28 12:41:19 +0300

    commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector(".gallery");
 
+
 galleryContainer.innerHTML = galleryItems
   .map(({ preview, original, description }) => {
     return `<a class = "gallery__item" href="${original}">
@@ -10,3 +11,11 @@ galleryContainer.innerHTML = galleryItems
     alt=${description}/></a>`;
   })
   .join("");
+
+let gallerySimple = new SimpleLightbox('.gallery__item')
+gallerySimple.on('show.simplelightbox', function () { 
+  gallerySimple.on('captionData.simplelightbox', function ()   
+   console.log(galleryItems.description))
+  
+})
+
